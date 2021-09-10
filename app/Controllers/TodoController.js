ba0e2fd3a459cd5346fcb909184f1aa3fdf6798c
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-10 16:19:11 -0600

    got everything wired up now just need to sync

---
+++ app/Controllers/TodoController.js
@@ -0,0 +1,15 @@
+import { ProxyState } from "../AppState.js";
+import { todoService } from "../Services/TodoService.js";
+
+
+
+
+
+
+export class TodoController {
+
+  constructor() {
+  
+  }
+}
+console.log('hi from todo controller')
